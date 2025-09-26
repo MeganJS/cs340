174f2a2403455fe0bd23f07007da2ce96a8b5359
MeganJS <247megamania@gmail.com>
2025-09-26 14:49:34 -0600

    created useUserInfo and useUserInfoActions hooks

--- tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {AuthToken, FakeData, Status, User} from "tweeter-shared";
 import Post from "./Post";
-import { UserInfoActionsContext, UserInfoContext } from "../userInfo/UserInfoContexts";
 import { useMessageActions } from "../toaster/MessageHooks";
+import { useUserInfo, useUserInfoActions } from "../userInfo/UserInfoHooks";
 
 interface Props {
     status: Status;
@@ -13,8 +12,8 @@ interface Props {
 const StatusItem = (props: Props) => {
     const { displayErrorMessage } = useMessageActions();
     const navigate = useNavigate();
-    const { displayedUser, authToken } = useContext(UserInfoContext);
-    const { setDisplayedUser } = useContext(UserInfoActionsContext);
+    const { displayedUser, authToken } = useUserInfo();
+    const { setDisplayedUser } = useUserInfoActions();
 
     const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
         event.preventDefault();
---
+++ tweeter-web-starter/tweeter-web/src/components/userInfo/UserInfoHooks.ts
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import { User, AuthToken } from "tweeter-shared"
+import { UserInfoActionsContext, UserInfoContext } from "./UserInfoContexts"
+
+
+interface UserInfoActions {
+  updateUserInfo: (
+    currentUser: User,
+    displayedUser: User | null,
+    authToken: AuthToken,
+    remember: boolean
+  ) => void,
+  clearUserInfo: () => void,
+  setDisplayedUser: (user: User) => void,
+}
+
+export const useUserInfoActions = (): UserInfoActions => {
+    const { updateUserInfo, clearUserInfo, setDisplayedUser } = useContext(UserInfoActionsContext);
+
+    return {
+        updateUserInfo: updateUserInfo,
+        clearUserInfo: clearUserInfo,
+        setDisplayedUser: setDisplayedUser
+    }
+}
+
+export const useUserInfo = () => {
+    return useContext(UserInfoContext);
+}
