4f04c27872638005430a4948c7deef5a66f91ca9
MeganJS <247megamania@gmail.com>
2025-11-07 14:33:54 -0700

    GetFolloweeCountLambda, GetFollowerCountLambda

---
+++ tweeter-web-starter/tweeter-server/src/lambda/user/GetFollowerCountLambda.ts
@@ -0,0 +1,19 @@
+import { FollowActionRequest, FollowCountResponse } from "tweeter-shared";
+import { UserService } from "../../model/service/UserService";
+
+export const handler = async (
+  request: FollowActionRequest
+): Promise<FollowCountResponse> => {
+  const userService: UserService = new UserService();
+
+  const followeeCount = await userService.getFollowerCount(
+    request.token,
+    request.user
+  );
+
+  return {
+    success: true,
+    message: null,
+    count: followeeCount,
+  };
+};
