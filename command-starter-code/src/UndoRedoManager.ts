ce069fe0534ba1331215febfeb91e717971a14e4
MeganJS <247megamania@gmail.com>
2025-11-22 19:39:16 -0700

    created UndoRedoManager

--- command-starter-code/src/UndoRedoManager.ts
+++ command-starter-code/src/UndoRedoManager.ts
@@ -1,4 +1,4 @@
-import { CommandStack } from "./Command";
+import { Command, CommandStack } from "./Command";
 
 export class UndoRedoManager {
   private undoStack: CommandStack;
@@ -9,9 +9,28 @@ export class UndoRedoManager {
     this.redoStack = new CommandStack();
   }
 
-  execute();
-  undo();
-  redo();
-  canUndo();
-  canRedo();
+  execute(command: Command) {
+    command.do();
+    this.undoStack.push(command);
+  }
+  undo() {
+    if (this.canUndo()) {
+      let command: Command = this.undoStack.pop()!;
+      command.undo();
+      this.redoStack.push(command);
+    }
+  }
+  redo() {
+    if (this.canRedo()) {
+      let command: Command = this.redoStack.pop()!;
+      command.do();
+      this.undoStack.push(command);
+    }
+  }
+  canUndo(): boolean {
+    return !this.undoStack.isEmpty();
+  }
+  canRedo() {
+    return !this.redoStack.isEmpty();
+  }
 }
--- command-starter-code/src/editor/TextEditor.ts
+++ command-starter-code/src/editor/TextEditor.ts
@@ -15,6 +15,10 @@ export class TextEditor {
     });
   }
 
+  public get document(): IDocument {
+    return this._document;
+  }
+
   run(): void {
     this.consoleReader.question(this.getOptions(), (input) => {
       const option = UserInputReader.validateNumberInput(input);
@@ -77,14 +81,15 @@ SELECT AN OPTION (1 - 10):
 
 Your selection: `;
   }
-
+  /*
   private insert(): void {
     const insertionInput = UserInputReader.getUserInput("Start index: ");
     const insertionIndex = UserInputReader.validateNumberInput(insertionInput);
     const sequenceInput = UserInputReader.getUserInput("Sequence to insert: ");
     this._document.insert(insertionIndex, sequenceInput);
   }
-
+    */
+  /*
   private delete(): void {
     const deletionIndexInput = UserInputReader.getUserInput("Start index: ");
     const deletionIndex =
@@ -102,7 +107,9 @@ Your selection: `;
       console.log("Deletion unsuccessful");
     }
   }
+    */
 
+  /*
   private replace(): void {
     const replaceIndexInput = UserInputReader.getUserInput("Start index: ");
     const replaceIndex = UserInputReader.validateNumberInput(replaceIndexInput);
@@ -127,6 +134,7 @@ Your selection: `;
       this._document.insert(replaceIndex, replacementString);
     }
   }
+    */
 
   private save(): void {
     const saveFileName = UserInputReader.getUserInput("Name of file: ");
@@ -140,16 +148,139 @@ Your selection: `;
     this._document.save(saveFileName);
   }
 
+  /*
   private open(): void {
     const openFileName = UserInputReader.getUserInput("Name of file to open: ");
     this._document.open(openFileName);
   }
+    */
 }
 
-/*
 class StartCommand implements Command {
-  
-}
-*/
+  private contents: string = "";
+  constructor(private editor: TextEditor) {}
 
-class InsertCommand implements Command {}
+  do() {
+    this.contents = this.editor.document.getContents();
+    this.editor.document.clear();
+  }
+
+  undo() {
+    this.editor.document.clear(); // is this correct?
+    this.editor.document.insert(0, this.contents);
+  }
+}
+
+class InsertCommand implements Command {
+  constructor(
+    private editor: TextEditor,
+    private insertionInput = UserInputReader.getUserInput("Start index: "),
+    private insertionIndex = UserInputReader.validateNumberInput(
+      this.insertionInput
+    ),
+    private sequenceInput = UserInputReader.getUserInput("Sequence to insert: ")
+  ) {}
+
+  do() {
+    this.editor.document.insert(this.insertionIndex, this.sequenceInput);
+  }
+
+  undo() {
+    this.editor.document.delete(this.insertionIndex, this.sequenceInput.length);
+  }
+}
+
+class DeleteCommand implements Command {
+  private deletedSequence: string = "";
+
+  constructor(
+    private editor: TextEditor,
+    private deletionIndexInput = UserInputReader.getUserInput("Start index: "),
+    private deletionIndex = UserInputReader.validateNumberInput(
+      this.deletionIndexInput
+    ),
+    private deletionDistanceInput = UserInputReader.getUserInput(
+      "Sequence to insert: "
+    ),
+    private deletionDistance = UserInputReader.validateNumberInput(
+      this.deletionDistanceInput
+    )
+  ) {}
+
+  do() {
+    this.deletedSequence = this.editor.document.delete(
+      this.deletionIndex,
+      this.deletionDistance
+    );
+    if (this.deletedSequence == null) {
+      console.log("Deletion unsuccessful");
+      this.deletedSequence = "";
+    }
+  }
+
+  undo() {
+    this.editor.document.insert(this.deletionIndex, this.deletedSequence);
+  }
+}
+
+class ReplaceCommand implements Command {
+  private replacedSequence: string = "";
+  private replaceDistance: number = 0;
+  constructor(
+    private editor: TextEditor,
+    private replaceIndexInput = UserInputReader.getUserInput("Start index: "),
+    private replaceIndex = UserInputReader.validateNumberInput(
+      this.replaceIndexInput
+    )
+  ) {}
+
+  do() {
+    //let replaceDistance: number = 0;
+    let replacementString: string = "";
+
+    if (this.replaceIndex != -1) {
+      const replaceDistanceInput = UserInputReader.getUserInput(
+        "Number of characters to replace: "
+      );
+      this.replaceDistance =
+        UserInputReader.validateNumberInput(replaceDistanceInput);
+
+      if (this.replaceDistance != -1) {
+        replacementString = UserInputReader.getUserInput(
+          "Replacement string: "
+        );
+      }
+
+      this.replacedSequence = this.editor.document.delete(
+        this.replaceIndex,
+        this.replaceDistance
+      );
+      if (this.replacedSequence == null) {
+        this.replacedSequence = "";
+      }
+      this.editor.document.insert(this.replaceIndex, replacementString);
+    }
+  }
+
+  undo() {
+    this.editor.document.delete(this.replaceIndex, this.replaceDistance);
+    this.editor.document.insert(this.replaceIndex, this.replacedSequence);
+  }
+}
+
+class OpenCommand implements Command {
+  constructor(
+    private editor: TextEditor,
+    private openFileName = UserInputReader.getUserInput(
+      "Name of file to open: "
+    )
+  ) {}
+
+  do() {
+    this.editor.document.open(this.openFileName);
+  }
+
+  undo() {
+    this.editor.document.save(this.openFileName);
+  }
+}
